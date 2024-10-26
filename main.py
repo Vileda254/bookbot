326c6e3fce4bc27089eb4565f44e4681a5006131
Jakub Golan <jakub.golan.ext@buildingminds.com>
2024-10-26 20:47:37 +0200

    Add proper functions for each task

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
-file = open("books/frankenstein.txt", "r")
+def main():
+    file_path = "books/frankenstein.txt"
+    print(character_count(file_path))
 
-content = file.read()
 
-words = content.split()
+def count_words(file_name):
+    with open(file_name, "r") as file:
+        content = file.read()
+        words = content.split()
+        return len(words)
+    
+def print_text(file_name):
+    with open(file_name, "r") as file:
+        content = file.read()
+        return content
+    
+def character_count(file_name):
+    with open(file_name, "r") as file:
+        content = file.read()
+        char_count = {}
+        for char in content:
+            char = char.lower()
+            if char in char_count:
+                char_count[char] += 1
+            else:
+                char_count[char] = 1
 
-word_count = len(words)
+        return char_count
+    
 
-print(word_count)
-
-file.close()
+main()
